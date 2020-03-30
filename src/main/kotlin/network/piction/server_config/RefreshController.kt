1bef79747cb0b3c72fdb1c5be8876cb6462ae9f0
sk.kwon <sk.kwon@battleent.com>
2020-03-30 15:39:04 +0900

    Fix body due to github webhooks issue

--- src/main/kotlin/network/piction/server_config/RefreshController.kt
+++ src/main/kotlin/network/piction/server_config/RefreshController.kt
@@ -2,7 +2,6 @@ package network.piction.server_config
 
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
-import org.springframework.util.MultiValueMap
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -16,7 +15,7 @@ class RefreshController {
     @PostMapping("/refresh")
     fun refresh(request: HttpServletRequest) = RestTemplate().postForEntity(
         request.requestURL.toString().replace(request.requestURI, "/actuator/bus-refresh"),
-        HttpEntity<MultiValueMap<String, String>?>(null, HttpHeaders().apply {
+        HttpEntity("", HttpHeaders().apply {
             this.add(HttpHeaders.CONTENT_TYPE, "application/json")
         }),
         String::class.java
